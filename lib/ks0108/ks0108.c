5c05456aad82b47b4849dc2826beba569327beae
Stanislav Lakhtin <savant@savant.pro>
2016-11-10 17:58:34 +0300

    delay

--- lib/ks0108/ks0108.c
+++ lib/ks0108/ks0108.c
@@ -4,5 +4,26 @@
 #include "ks0108.h"
 
 void ks0108_init() {
-
+    frame f;
+    f.ks0108.res = 0;
+    ks0108_sendByte(f); //we MUST wait at least 1 mks after that we should wait busy/reset clear in ic or wait 10 mks
+    delayMs(1);
+    f.ks0108.res = 1;
+    ks0108_sendByte(f);
+    delayMs(10);
 }
+
+/* sleep for delay milliseconds */
+static void delayMs(uint32_t mks)
+{
+    uint32_t t = 0;
+    uint32_t delay = 720*mks; //todo переделать потом от частоты и таймеров
+    for (t = 0; t < delay; t++)
+       __asm__("nop");
+}
+
+void ks0108_sendByte(frame f) {
+    gpio_set(GPIOA, f.data);
+}
+
+
--- lib/ks0108/ks0108.h
+++ lib/ks0108/ks0108.h
@@ -1,13 +1,32 @@
 //
 // Created by sl on 09.11.16.
 //
-
 #ifndef TMONITOR_KS0108_H
 #define TMONITOR_KS0108_H
 
-uint8_t ks0108_buffer[16];
+#include <libopencm3/stm32/gpio.h>
+
+uint8_t ks0108_buffer[128][2];
+
+union registerGpio {
+    uint16_t data;
+    struct {
+        unsigned unused: 2;
+        unsigned en: 1;
+        unsigned cmd: 1;
+        unsigned rw:1;
+        unsigned res: 1;
+        unsigned cs: 2;
+        unsigned data: 8;
+    } ks0108;
+};
+
+//#define DISPLAY_ON {0,0,}
+
+typedef union registerGpio frame;
 
 void ks0108_init();
-void ks0108_sendByte(uint8_t);
+void ks0108_sendByte(frame);
+static void delayMs(uint32_t mks);
 
 #endif //TMONITOR_KS0108_H
