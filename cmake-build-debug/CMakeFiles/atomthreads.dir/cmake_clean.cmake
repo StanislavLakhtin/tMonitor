ea6ea9fac4524868e0bf4375e67f2a2d9844ca1a
Stanislav Lakhtin <savant@savant.pro>
2016-12-25 19:53:47 +0300

    .gitignore for cmake-build-debug

--- cmake-build-debug/CMakeFiles/atomthreads.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/atomthreads"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/atomthreads.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- cmake-build-debug/src/CMakeFiles/tMonitor.bin.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/tMonitor.bin"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/tMonitor.bin.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
